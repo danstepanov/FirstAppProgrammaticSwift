20f6e0da04d461a548908b002cbc8234b5ddb5a6
danstepanov <danhstepanov@gmail.com>
2016-02-07 23:21:46 -0800

    added text field

--- FirstAppProgrammaticSwift/MainViewController.swift
+++ FirstAppProgrammaticSwift/MainViewController.swift
@@ -8,28 +8,68 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        textField.delegate = self
+        view.addSubview(textField)
+        view.setNeedsUpdateConstraints()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        // Dismisses the Keyboard by making the text field resign
+        // first responder
+        textField.resignFirstResponder()
+        
+        // returns false. Instead of adding a line break, the text
+        // field resigns
+        return false
     }
-    */
+    
+    override func updateViewConstraints() {
+        textFieldConstraint()
+        super.updateViewConstraints()
+    }
+    
+    func textFieldConstraint() {
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .CenterX,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .CenterX,
+            multiplier: 1.0,
+            constant: 0.0)
+            .active = true
+        
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .Width,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .Width,
+            multiplier: 0.8,
+            constant: 0.0)
+            .active = true
+        
+        NSLayoutConstraint(
+            item: textField,
+            attribute: .Top,
+            relatedBy: .Equal,
+            toItem: view,
+            attribute: .Bottom,
+            multiplier: 0.1,
+            constant: 0.0)
+            .active = true
+    }
+
+    lazy var textField: UITextField! = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.borderStyle = .RoundedRect
+        return view
+    }()
 
 }
